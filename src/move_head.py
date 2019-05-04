0798a27b9910f1eda7c051378a0dd7635c17299a
Arne Sachtler <arne.sachtler@gmail.com>
2019-05-04 19:26:44 +0200

    Add move head node

--- src/move_head.py
+++ src/move_head.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
 
-
 import rospy
+from std_msgs.msg import Float32
+
+idle_pos = 0
+max_pos = 10
+start_threshold = 1.0
+axis = '1'
+
+
+class MoveHead(object):
+	def __init__(self):
+		rospy.init_node('move_head')
+		rospy.Subscriber('shy_roboy/nearest_distance', Float32, self.process_distance_measure)
+		self._head_motion = rospy.Publisher('sphere_axis{0}/sphere_axis{0}/target'.format(axis), Float32, queue_size=1)
+
+	def process_distance_measure(self, data):
+		angle = max(0, start_threshold - data.data) * max_pos + idle_pos
+		self._head_motion.publish(angle)
+		rospy.loginfo('Angle: {}'.format(angle))
+
+
+if __name__ == '__main__':
+	mh = MoveHead()
+	rospy.spin()
