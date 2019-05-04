fc1f13aaa5a76f06a63000b89d23578d6135b26b
Ruofan Xu <ruofan.xu@tum.de>
2019-05-04 15:15:10 +0200

    publishing distance (still need optimization)

--- scripts/getCamImg.py
+++ scripts/getCamImg.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import rospy
+import math
 import cv2
-from std_msgs.msg import String
+import numpy as np
+from std_msgs.msg import String, Float32
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -11,17 +13,35 @@ class image_converter:
     def __init__(self, *args, **kwargs):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/zed/zed_node/depth/depth_registered", Image, self.callback)
+        self.distance_pub = rospy.Publisher("shy_roboy/nearest_distance", Float32,queue_size=10)
+        self.cv_image = []
+        self.callback_received = False
 
     def callback(self,data):
-        rospy.loginfo("Depth image received")
+        # rospy.loginfo("Depth image received")
 
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
-            cv2.imshow('depth_image',cv_image)
+            self.cv_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
+
+            cv2.imshow('depth_image',self.cv_image)
             cv2.waitKey(3)
+
+            self.callback_received = True
 
         except CvBridgeError as e:
             print(e)
+
+    def getDepth(self):
+        height, width = self.cv_image.shape
+        depth = self.cv_image[(int)(height/2),(int)(width/2)]
+        #rospy.loginfo("height, width: {},{}".format(height,width))
+
+
+        #rospy.loginfo("depth of center: {}".format(depth))
+        
+        #publish if distance is not nan
+        if (not math.isnan(depth) and not np.isinf(depth)):
+            self.distance_pub.publish(depth)
 
 
         
@@ -32,9 +52,15 @@ def main():
     
     # cv2.namedWindow('depth_image', cv2.WINDOW_NORMAL)
     rospy.init_node('image_converter', anonymous=True)
+    rospy.loginfo("Node running.")
+    rate = rospy.Rate(10)
     try:
-        rospy.loginfo("Node running.")
-        rospy.spin()
+        while not rospy.is_shutdown():
+
+            if ic.callback_received:
+                ic.getDepth()
+
+            rate.sleep()
     except KeyboardInterrupt:
         print("Shutting down")
     cv2.destroyAllWindows()
