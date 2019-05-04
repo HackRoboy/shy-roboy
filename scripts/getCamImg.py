b9202bc537945ff98710bed6b3d2aeb9e0d44b9b
Ruofan Xu <ruofan.xu@tum.de>
2019-05-04 18:24:21 +0200

    Revert "working distance publisher. yay."
    This reverts commit df744bdcad65b2e8b5b1c9dccd484ba4483e1cd0.

--- scripts/getCamImg.py
+++ scripts/getCamImg.py
@@ -23,8 +23,8 @@ class image_converter:
         try:
             self.cv_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
 
-            # cv2.imshow('depth_image',self.cv_image)
-            # cv2.waitKey(3)
+            cv2.imshow('depth_image',self.cv_image)
+            cv2.waitKey(3)
 
             self.callback_received = True
 
@@ -33,41 +33,10 @@ class image_converter:
 
     def getDepth(self):
         height, width = self.cv_image.shape
-        h_new = height*0.3
-        w_new = width*0.3
-
-        resized = cv2.resize(self.cv_image, (int(w_new),int(h_new)), interpolation = cv2.INTER_AREA)
-        
-        cv2.imshow('resized_image',resized)
-        cv2.waitKey(3)
-
-        th = np.zeros(resized.shape[:2], np.uint8)
-
-        _,th = cv2.threshold(resized,0.7,1, cv2.THRESH_BINARY_INV)
-        cv2.imshow("thresh img",th)
-        cv2.waitKey(2)
-
-        #scale it up and back to calculate mean
-
-
-        # sum()
-        # depth = cv2.mean(resized, th)
-
-        # rospy.loginfo("mean depth: {}".format(depth))
-        
-        if(sum(sum(th))==0):
-            depth = 100.0
-        else:
-            depth = sum(resized[th==1])/sum(sum(th))
-        # except Exception as e:
-        #     print(e)
-        #     depth = 100.0
-
-        rospy.loginfo("mean depth: {}".format(depth))
-        #depth = resized[int(h_new/2),int(w_new/2)]
-        
-        
+        depth = self.cv_image[(int)(height/2),(int)(width/2)]
         #rospy.loginfo("height, width: {},{}".format(height,width))
+
+
         #rospy.loginfo("depth of center: {}".format(depth))
         
         #publish if distance is not nan
