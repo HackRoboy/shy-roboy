637897afd134b4fe4fbb971abf428e702fe2681f
stlukyanenko <stlukyanenko@gmail.com>
2019-05-04 21:02:03 +0200

    fix

--- src/shouter.py
+++ src/shouter.py
@@ -9,10 +9,15 @@ last_state = 0
 current_state = 0
 
 def process_state(data):
+    global current_state
+    global last_state
+    current_state = data.data
     if current_state == 2 and last_state != 2:
         shout()
+    last_state = current_state
 
 def shout():
+    global shout_node
     shout_node.getlogger().info('\tSHOUT')
     client = shout_node.create_client(Talk,'roboy/cognition/speech/synthesis/talk')
     request = Talk.Request()
@@ -21,6 +26,7 @@ def shout():
     rclpy.spin_until_future_complete(shout_node, future)
 
 def main(args=None):
+    global shout_node
     rclpy.init()
     shout_node = rclpy.create_node('shouter')
     shout_node.create_subscription(Int8, 'shy_roboy/state',  process_state)
