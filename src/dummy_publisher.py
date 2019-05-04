92c0da5e9748ad912856fa4646021c201b8e0dc0
BVURAL <bilal_v@hotmail.com>
2019-05-04 17:53:48 +0200

    fix typo

--- src/dummy_publisher.py
+++ src/dummy_publisher.py
@@ -1,9 +1,10 @@
 import rospy
 from std_msgs.msg import Float32
-from random import randint
+from random import gauss
+import time
 
 
-ROS_PUBLISHER_NAME = 'distance_publisher'
+ROS_PUBLISHER_NAME = 'shy_roboy/nearest_distance'
 
 def talker():
     rospy.loginfo('Initializing and starting ' + ROS_PUBLISHER_NAME + ' topic.')
@@ -12,7 +13,7 @@ def talker():
     publisher = rospy.Publisher(ROS_PUBLISHER_NAME, Float32, queue_size=10)
     
     # Initialization
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('dummy_distance_publisher')
 
     # Communication rate
     rate = rospy.Rate(60) # 60 Hz = 30 FPS
@@ -20,7 +21,9 @@ def talker():
     rospy.loginfo(ROS_PUBLISHER_NAME + ' topic has been initialized.')
 
     while not rospy.is_shutdown():
-        distance = randint(0, 9)
+
+        mean = 0.5 if int(time.time()) % 10 > 5 else 3.0
+        distance = mean + gauss(0, 0.1)
         rospy.loginfo(distance)
         publisher.publish(distance)
         rate.sleep()
