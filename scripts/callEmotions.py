d37f245e4e6a8bc5545a709d084c5ec6df075918
Arne Sachtler <arne.sachtler@gmail.com>
2019-05-04 21:46:08 +0200

    Listen to state topic

--- scripts/callEmotions.py
+++ scripts/callEmotions.py
@@ -3,20 +3,29 @@
 import sys
 import rospy
 from roboy_control_msgs.srv import ShowEmotion
-from std_msgs.msg import String
+from std_msgs.msg import Int8
 
-
-def main():
-    rospy.wait_for_service('/roboy/cognition/face/emotion')
-    try:
+class EmotionReation(object):
+	def __init__(self):
+	    rospy.wait_for_service('/roboy/cognition/face/emotion')
+		rospy.init_node('emotion_reaction')
+		rospy.Subscriber('shy_roboy/state', Int8, self.process_state)
         face_emotion = rospy.ServiceProxy('/roboy/cognition/face/emotion', ShowEmotion)
-        response = face_emotion("emotion: 'angry'")
-        print("Response: {}".format(response.success))
-        return True
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
 
+
+	def process_state(self, data):
+		current_state = data.data
+
+		if current_state in [2, 3]:
+			call_emotion_service('angry')
+
+	def call_emotion_service(self, emotion):
+		try:
+			response = self.face_emotion("emotion", emotion)
+		except rospy.ServiceException, e:
+	        print "Service call failed: %s"%e
 
 
 if __name__ == "__main__":
-    main()
+    er = EmotionReation()
+    rospy.spin()
