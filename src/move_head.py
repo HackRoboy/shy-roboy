b5dd09b60ff70c6111ade02f16cd42bbcc444aed
Arne Sachtler <arne.sachtler@gmail.com>
2019-05-05 13:43:11 +0200

    Fix

--- src/move_head.py
+++ src/move_head.py
@@ -6,7 +6,6 @@ from std_msgs.msg import Float32
 idle_pos = 0 # in radians
 max_pos = -0.3 # in radians
 start_threshold = 1.0
-axis = '1'
 
 
 class MoveHead(object):
@@ -15,7 +14,7 @@ class MoveHead(object):
 		# and publisher to sphere_axis1/sphere_axis1/target
 		rospy.init_node('move_head')
 		rospy.Subscriber('shy_roboy/nearest_distance', Float32, self.process_distance_measure)
-		self._head_motion = rospy.Publisher('sphere_axis{0}/sphere_axis{0}/target'.format(axis), Float32, queue_size=1)
+		self._head_motion = rospy.Publisher('sphere_axis1/sphere_axis1/target'.format(axis), Float32, queue_size=1)
 
 	def process_distance_measure(self, data):
 		# Calculate the angle in radians.
