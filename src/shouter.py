dc63b31ef239f80e4736ecfff012cd737ff16959
stlukyanenko <stlukyanenko@gmail.com>
2019-05-05 14:45:45 +0200

    thanking added

--- src/shouter.py
+++ src/shouter.py
@@ -14,6 +14,8 @@ def process_state(data):
     current_state = data.data
     if current_state == 2 and last_state != 2:
         shout()
+    if current_state == 0 and last_state == 3:
+        thank()
     last_state = current_state
 
 def shout():
@@ -24,7 +26,15 @@ def shout():
     print("Trying to shout. Hope someone hears me")
     future = client.call_async(request) 
     #rclpy.spin_until_future_complete(shout_node, future)
-
+    
+def thank()
+    global shout_node
+    client = shout_node.create_client(Talk,'roboy/cognition/speech/synthesis/talk')
+    request = Talk.Request()
+    request.text = "Thanks for leaving me alone"
+    print("Trying to thank. Hope someone hears me")
+    future = client.call_async(request) 
+    
 rclpy.init()
 shout_node = rclpy.create_node('shouter')
 shout_node.create_subscription(Int8, 'shy_roboy/state',  process_state)
