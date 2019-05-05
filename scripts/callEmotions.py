2706cbb86060014b3766e0575a0d71c9c00f5292
BVURAL <bilal_v@hotmail.com>
2019-05-05 10:17:02 +0200

    added delay between emotions calls

--- scripts/callEmotions.py
+++ scripts/callEmotions.py
@@ -3,10 +3,13 @@
 import rospy
 from roboy_control_msgs.srv import ShowEmotion
 from std_msgs.msg import Int8
+import time
 
 
 class EmotionReaction(object):
     def __init__(self):
+        self.last_publish_time = None
+        self.sleep_time_after_execution = 5
         rospy.wait_for_service('/roboy/cognition/face/emotion')
         rospy.init_node('emotion_reaction')
         rospy.Subscriber('shy_roboy/state', Int8, self.process_state)
@@ -21,8 +24,11 @@ class EmotionReaction(object):
 
     def call_emotion_service(self, emotion):
         try:
-            response = self.face_emotion(emotion)
-            print(response)
+            if self.last_publish_time is None:
+                self.last_publish_time = time.time()
+            elif (time.time() - self.last_publish_time) > self.sleep_time_after_execution:
+                response = self.face_emotion(emotion)
+                print(response)
         except rospy.ServiceException, e:
             print "Service call failed: %s" % e
 
