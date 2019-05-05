2186e4281c2c9e251b1da7bccc838292d3e6983c
BVURAL <bilal_v@hotmail.com>
2019-05-05 11:38:05 +0200

    added comments to codes

--- scripts/getCamImg.py
+++ scripts/getCamImg.py
@@ -8,9 +8,13 @@ from std_msgs.msg import String, Float32
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+# This script listens /zed/zed_node/depth/depth_registered
+# and calculates the average distance of the closest objects.
+# Then, publishes average distance to shy_roboy/nearest_distance.
 
 class ImageConverter:
     def __init__(self):
+        # Initialize depth image listener and average distance publisher.
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/zed/zed_node/depth/depth_registered", Image, self.callback)
         self.distance_pub = rospy.Publisher("shy_roboy/nearest_distance", Float32, queue_size=10)
@@ -19,6 +23,7 @@ class ImageConverter:
 
     def callback(self, data):
         try:
+            # Read depth image.
             self.cv_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
             self.callback_received = True
         except CvBridgeError as e:
@@ -30,23 +35,28 @@ class ImageConverter:
         h_new = height * 0.3
         w_new = width * 0.3
 
+        # Resize image to process faster.
         resized = cv2.resize(self.cv_image, (int(w_new), int(h_new)), interpolation=cv2.INTER_AREA)
 
-        cv2.imshow('Resized Image', resized)
-        cv2.waitKey(1)
-
+        # Discard everything that is beyond 0.7 meters.
         _, th = cv2.threshold(resized, 0.7, 1, cv2.THRESH_BINARY_INV)
+
+        # Show images.
+        cv2.imshow('Resized Image', resized)
         cv2.imshow('Thresholded Image', th)
         cv2.waitKey(1)
 
+        # If everything is too far,
+        # send a big number.
         if sum(sum(th)) == 0:
             depth = 100.0
+        # Else, calculate average distance of white pixels.
         else:
             depth = sum(resized[th == 1]) / sum(sum(th))
 
         rospy.loginfo("mean depth: {}".format(depth))
 
-        # publish if distance is not nan
+        # If average distance is not nan or inf, publish it.
         if not math.isnan(depth) and not np.isinf(depth):
             self.distance_pub.publish(depth)
 
@@ -57,7 +67,8 @@ def main():
     rospy.init_node('image_converter', anonymous=True)
     rospy.loginfo("Node running.")
 
-    rate = rospy.Rate(30)
+    # 30 FPS = 60 Hz
+    rate = rospy.Rate(60)
 
     try:
         while not rospy.is_shutdown():
--- src/move_head.py
+++ src/move_head.py
@@ -3,19 +3,22 @@
 import rospy
 from std_msgs.msg import Float32
 
-idle_pos = 0 # rad
-max_pos = 0.5 # rad
+idle_pos = 0 # in radians
+max_pos = 0.5 # in radians
 start_threshold = 1.0
 axis = '1'
 
 
 class MoveHead(object):
 	def __init__(self):
+		# Initialize the listener to shy_roboy/nearest_distance
+		# and publisher to sphere_axis1/sphere_axis1/target
 		rospy.init_node('move_head')
 		rospy.Subscriber('shy_roboy/nearest_distance', Float32, self.process_distance_measure)
 		self._head_motion = rospy.Publisher('sphere_axis{0}/sphere_axis{0}/target'.format(axis), Float32, queue_size=1)
 
 	def process_distance_measure(self, data):
+		# Calculate the angle in radians.
 		angle = max(0, start_threshold - data.data) * max_pos + idle_pos
 		self._head_motion.publish(angle)
 		rospy.loginfo('Angle: {}'.format(angle))
